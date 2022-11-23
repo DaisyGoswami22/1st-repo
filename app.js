c299a6551c6606c9bc82cb084e8cd3683b15cff2
Bikramjeet Sarmah <9854bikram@gmail.com>
2022-11-23 12:30:48 +0530

    routes added

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.use(cookieParser());
 app.use(cors());
 
 // routes
+const userRoutes = require("./routes/userRoutes");
+const studentRoutes = require("./routes/studentRoutes");
+const subjectRoutes = require("./routes/subjectRoutes");
+const resultRoutes = require("./routes/resultRoutes");
+
+app.use("/api/v1", userRoutes);
+app.use("/api/v1", studentRoutes);
+app.use("/api/v1", subjectRoutes);
+app.use("/api/v1", resultRoutes);
 
 // middleware for error
 app.use(errorMiddleware);
